06c224a8013068ae48cad28da1422c7a5fbad950
Terry Moore <tmm@mcci.com>
2018-02-17 22:14:50 -0500

    Clean up and document slightly

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # catena4460-demo1
-Aruduino demo sketch for the Catena 4460
+
+Aruduino demo sketch for the MCCI Catena&reg; 4460.
+
+This sketch demonstrates Catena 4460 as a LoRaWAN remote sensor.  The device periodically measures temperature, pressure, humidity, light, and atmospheric resistance; the resulting data is transmitted to the cloud as a packed binary message.
+
+When building, please select Catena 4460 as the target board in the Arduino IDE.
--- catena4460-demo1.ino
+++ catena4460-demo1.ino
@@ -1,41 +1,31 @@
-/* catena4450m101_sensor.ino	Tue Mar 28 2017 19:52:20 tmm */
+/* catena4460-demo1.ino	Tue Mar 28 2017 19:52:20 tmm */
 
 /*
 
-Module:  catena4450m101_sensor.ino
+Module:  catena4460-demo1.ino
 
 Function:
-	Code for the electric sensor with Catena 4450-M101.
-
-Version:
-	V0.1.1	Tue Mar 28 2017 19:52:20 tmm	Edit level 1
+	Demonstrate the Catena 4460's built-in sensors.
 
 Copyright notice:
-	This file copyright (C) 2017 by
+	This file copyright (C) 2017-2018 by
 
 		MCCI Corporation
 		3520 Krums Corners Road
 		Ithaca, NY  14850
 
-	An unpublished work.  All rights reserved.
-
-	This file is proprietary information, and may not be disclosed or
-	copied without the prior permission of MCCI Corporation.
+	An unpublished work.  All rights reserved. The license file
+        accompanying this file outlines the license granted.
 
 Author:
-	Terry Moore, MCCI Corporation	March 2017
+	Terry Moore, MCCI Corporation & Suzen Filke	December 2018
 
-Revision history:
-   0.1.0  Fri Mar 10 2017 21:42:21  tmm
-	Module created.
-
-   0.1.1  Tue Mar 28 2017 19:52:20  tmm
-	Fix bug: not reading current lux value because sensor was not in
-	continuous mode. Add 150uA of current draw.
+Version and revision:
+        See https://github.com/mcci-catena/catena4460-demo1
 
 */
 
-#include <Catena4450.h>
+#include <Catena.h>
 #include <CatenaRTC.h>
 #include <Catena_Led.h>
 #include <Catena_TxBuffer.h>
@@ -64,7 +54,6 @@ Revision history:
 \****************************************************************************/
 
 using namespace McciCatena;
-using ThisCatena = Catena4450;
 
 /* how long do we wait between measurements (in seconds) */
 enum    {
@@ -72,6 +61,10 @@ enum    {
         // Actual time will be a little longer because have to
         // add measurement and broadcast time.
         CATCFG_T_CYCLE = 30,        // every 30 seconds
+        };
+
+/* additional timing parameters */
+enum    {
         CATCFG_T_WARMUP = 1,
         CATCFG_T_SETTLE = 5,
         CATCFG_T_INTERVAL = CATCFG_T_CYCLE - (CATCFG_T_WARMUP +
@@ -94,7 +87,7 @@ static Arduino_LoRaWAN::SendBufferCbFn sendBufferDoneCb;
 |
 \****************************************************************************/
 
-static const char sVersion[] = "0.1.7";
+static const char sVersion[] = "0.1.1";
 
 /****************************************************************************\
 |
@@ -110,19 +103,19 @@ static const char sVersion[] = "0.1.7";
 \****************************************************************************/
 
 // globals
-ThisCatena gCatena;
+Catena gCatena;
 
 //
 // the LoRaWAN backhaul.  Note that we use the
-// ThisCatena version so it can provide hardware-specific
+// Catena version so it can provide hardware-specific
 // information to the base class.
 //
-ThisCatena::LoRaWAN gLoRaWAN;
+Catena::LoRaWAN gLoRaWAN;
 
 //
 // the LED
 //
-StatusLed gLed (ThisCatena::PIN_STATUS_LED);
+StatusLed gLed (Catena::PIN_STATUS_LED);
 
 // the RTC instance, used for sleeping
 CatenaRTC gRtc;
@@ -135,25 +128,9 @@ bool fBme;
 BH1750 bh1750;
 bool fLux;
 
-//   The contact sensors
-bool fHasPower1;
-uint8_t kPinPower1P1;
-uint8_t kPinPower1P2;
-
-cTotalizer gPower1P1;
-cTotalizer gPower1P2;
-
-//  the job that's used to synchronize us with the LMIC code
+//  the LMIC job that's used to synchronize us with the LMIC code
 static osjob_t sensorJob;
 void sensorJob_cb(osjob_t *pJob);
-
-// function for scaling power
-static uint16_t
-dNdT_getFrac(
-        uint32_t deltaC,
-        uint32_t delta_ms
-        );
-
 
 /*
 
@@ -203,14 +180,14 @@ void setup(void)
                 gCatena.registerObject(&gLoRaWAN);
                 }
 
-        ThisCatena::UniqueID_string_t CpuIDstring;
+        Catena::UniqueID_string_t CpuIDstring;
 
         gCatena.SafePrintf("CPU Unique ID: %s\n",
                 gCatena.GetUniqueIDstring(&CpuIDstring)
                 );
 
         /* find the platform */
-        const ThisCatena::EUI64_buffer_t *pSysEUI = gCatena.GetSysEUI();
+        const Catena::EUI64_buffer_t *pSysEUI = gCatena.GetSysEUI();
 
         uint32_t flags;
         const CATENA_PLATFORM * const pPlatform = gCatena.GetPlatform();
@@ -267,56 +244,17 @@ void setup(void)
         /* is it modded? */
         uint32_t modnumber = gCatena.PlatformFlags_GetModNumber(flags);
 
-        fHasPower1 = false;
-
         if (modnumber != 0)
                 {
-                gCatena.SafePrintf("Catena 4450-M%u\n", modnumber);
-                if (modnumber == 101)
-                        {
-                        fHasPower1 = true;
-                        kPinPower1P1 = A0;
-                        kPinPower1P2 = A1;
-                        }
-                else
-                        {
-                        gCatena.SafePrintf("unknown mod number %d\n", modnumber);
-                        }
+                gCatena.SafePrintf("Catena 4460-M%u\n", modnumber);
                 }
         else
                 {
                 gCatena.SafePrintf("No mods detected\n");
                 }
 
-        if (fHasPower1)
-                {
-                if (! gPower1P1.begin(kPinPower1P1) ||
-                    ! gPower1P2.begin(kPinPower1P2))
-                        {
-                        fHasPower1 = false;
-                        }
-                }
-
         /* now, we kick off things by sending our first message */
         gLed.Set(LedPattern::Joining);
-
-        // unit testing for the scaling functions
-        //gCatena.SafePrintf(
-        //        "dNdT_getFrac tests: "
-        //        "0/0: %04x 90/6m: %04x 89/6:00.1: %04x 1439/6m: %04x\n",
-        //        dNdT_getFrac(0, 0),
-        //        dNdT_getFrac(90, 6 * 60 * 1000),
-        //        dNdT_getFrac(89, 6 * 60 * 1000 + 100),
-        //        dNdT_getFrac(1439, 6 * 60 * 1000)
-        //        );
-        //gCatena.SafePrintf(
-        //        "dNdT_getFrac tests: "
-        //        "1/6m: %04x 20/6m: %04x 1/60:00.1: %04x 1440/5:59.99: %04x\n",
-        //        dNdT_getFrac(1, 6 * 60 * 1000),
-        //        dNdT_getFrac(20, 6 * 60 * 1000),
-        //        dNdT_getFrac(1, 60 * 60 * 1000 + 100),
-        //        dNdT_getFrac(1440, 6 * 60 * 1000 - 10)
-        //        );
 
         /* warm up the BME680 by discarding a measurement */
         if (fBme)
@@ -334,40 +272,6 @@ void loop()
         gCatena.poll();
         }
 
-static uint16_t dNdT_getFrac(
-        uint32_t deltaC,
-        uint32_t delta_ms
-        )
-        {
-        if (delta_ms == 0 || deltaC == 0)
-                return 0;
-
-        // this is a value in [0,1)
-        float dNdTperHour = float(deltaC * 250) / float(delta_ms);
-
-        if (dNdTperHour <= 0)
-                return 0;
-        else if (dNdTperHour >= 1)
-                return 0xFFFF;
-        else
-                {
-                int iExp;
-                float normalValue;
-                normalValue = frexpf(dNdTperHour, &iExp);
-
-                // dNdTperHour is supposed to be in [0..1), so useful exp
-                // is [0..-15]
-                iExp += 15;
-                if (iExp < 0)
-                        iExp = 0;
-                if (iExp > 15)
-                        return 0xFFFF;
-
-
-                return (uint16_t)((iExp << 12u) + (unsigned) scalbnf(normalValue, 12));
-                }
-        }
-
 void startSendingUplink(void)
 {
   TxBuffer_t b;
@@ -379,8 +283,13 @@ void startSendingUplink(void)
   flag = FlagsSensor2(0);
 
   b.put(FormatSensor2); /* the flag for this record format */
+  
+  /* capture the address of the flag byte */
   uint8_t * const pFlag = b.getp();
-  b.put(0x00); /* will be set to the flags */
+  
+  b.put(0x00);  /* insert a byte. Value doesn't matter, will 
+                || later be set to the final value of `flag`
+                */
 
   // vBat is sent as 5000 * v
   float vBat = gCatena.ReadVbat();
@@ -397,7 +306,9 @@ void startSendingUplink(void)
 
   if (fBme)
        {
-       // Adafruit_BME280::Measurements m = bme.readTemperaturePressureHumidity();
+       // bme.performReading() loads a consistent measurement into 
+       // the bme. 
+       //
        // temperature is 2 bytes from -0x80.00 to +0x7F.FF degrees C
        // pressure is 2 bytes, hPa * 10.
        // humidity is one byte, where 0 == 0/256 and 0xFF == 255/256.
@@ -424,59 +335,6 @@ void startSendingUplink(void)
         gCatena.SafePrintf("BH1750:  %u lux\n", light);
         b.putLux(light);
         flag |= FlagsSensor2::FlagLux;
-        }
-
-  if (fHasPower1)
-        {
-        uint32_t power1in, power1out;
-        uint32_t power1in_dc, power1in_dt;
-        uint32_t power1out_dc, power1out_dt;
-
-        power1in = gPower1P1.getcurrent();
-        gPower1P1.getDeltaCountAndTime(power1in_dc, power1in_dt);
-        gPower1P1.setReference();
-
-        power1out = gPower1P2.getcurrent();
-        gPower1P2.getDeltaCountAndTime(power1out_dc, power1out_dt);
-        gPower1P2.setReference();
-
-        gCatena.SafePrintf(
-                "Power:   IN: %u OUT: %u\n",
-                power1in,
-                power1out
-                );
-        b.putWH(power1in);
-        b.putWH(power1out);
-        flag |= FlagsSensor2::FlagWattHours;
-
-        // we know that we get at most 4 pulses per second, no matter
-        // the scaling. Therefore, if we convert to pulses/hour, we'll
-        // have a value that is no more than 3600 * 4, or 14,400.
-        // This fits in 14 bits. At low pulse rates, there's more
-        // info in the denominator than in the numerator. So FP is really
-        // called for. We use a simple floating point format, since this
-        // is unsigned: 4 bits of exponent, 12 bits of fraction, and we
-        // don't bother to omit the MSB of the (normalized fraction);
-        // and we multiply by 2^(exp+1) (so 0x0800 is 2 * 0.5 == 1,
-        // 0xF8000 is 2^16 * 1 = 65536).
-        uint16_t fracPower1In = dNdT_getFrac(power1in_dc, power1in_dt);
-        uint16_t fracPower1Out = dNdT_getFrac(power1out_dc, power1out_dt);
-        b.putPulseFraction(
-                fracPower1In
-                );
-        b.putPulseFraction(
-                fracPower1Out
-                );
-
-        gCatena.SafePrintf(
-                "Power:   IN: %u/%u (%04x) OUT: %u/%u (%04x)\n",
-                power1in_dc, power1in_dt,
-                fracPower1In,
-                power1out_dc, power1out_dt,
-                fracPower1Out
-                );
-
-        flag |= FlagsSensor2::FlagPulsesPerHour;
         }
 
   *pFlag = uint8_t(flag);
@@ -544,8 +402,8 @@ static void settleDoneCb(
     // ditto if we're monitoring pulses.
 
     // enable/disable sleep when connected to USB
-    //if (Serial.dtr() || fHasPower1)
-    if (Serial.dtr() | fHasPower1 || true)
+    //if (Serial.dtr())
+    if (Serial.dtr() || true)
         {
         gLed.Set(LedPattern::Sleeping);
         os_setTimedCallback(
